7cfbd3568ed642e3e92c8546daaed32789c77d1b
yx <yxue3357@qq.com>
2022-09-20 03:10:33 +0800

    emacs onfig init

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,1 +1,6 @@
-(menu-bar-mode -1)
+;; -*- lexical-binding: t -*-
+
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+
+(setq custom-file (expand-file-name "~/.emacs.d/custom.el"))
+(load custom-file 'no-error 'no-message)
